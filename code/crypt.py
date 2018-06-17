83dceff4a8518a348838c80227c73d6352719596
sbyzer <40241000+sbyzer@users.noreply.github.com>
2018-06-17 20:38:10 +0200

    added generated script

---
+++ code/crypt.py
@@ -0,0 +1,17 @@
+# GENERATED WITH: 'angecryption.py'
+
+# This script decrypts the file 'zipflv.zip' that is located
+# in 'result/ZIPFLV' with the key 'MySuperSecureKey' and writes
+# the result into the file 'zipflv.flv' that will also be located
+# in 'result/ZIPFLV'.
+from Crypto.Cipher import AES
+
+algo = AES.new('MySuperSecureKey', AES.MODE_CBC, '\x1d\x9e\xa6\xd1\t0\xb04\x91\x95\xf9\xf2[\\\x8f\xc6')
+
+with open('../result/ZIPFLV/zipflv.zip', 'rb') as f:
+	d = f.read()
+
+d = algo.encrypt(d)
+
+with open('../result/ZIPFLV/zipflv.flv', 'wb') as f:
+	f.write(d)
